b72e68e6e1f2891c2a7c600ab5097c52a008db21
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-08 14:25:40 +0300

    Fruit

---
+++ src/Interface/Fruit.java
@@ -0,0 +1,9 @@
+package Interface;
+
+import java.util.ArrayList;
+
+public abstract class Fruit {
+	private String color;
+	public abstract ArrayList<String> getVitamin();
+	public abstract String getColor();
+}
