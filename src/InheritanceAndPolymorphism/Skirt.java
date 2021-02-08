da0433c59bd05a25af7cd647d0e41f57147399b9
GokselOnal <73059230+GokselOnal@users.noreply.github.com>
2021-02-08 14:21:19 +0300

    Cloth products

---
+++ src/InheritanceAndPolymorphism/Skirt.java
@@ -0,0 +1,9 @@
+package InheritanceAndPolymorphism;
+
+public class Skirt extends Cloth{
+	public Skirt(int quantity, String brand, String color, double taxRate, String name) {
+		super(taxRate,color,brand);
+		this.setQuantity(quantity);
+		this.setName(name);	
+	}
+}
